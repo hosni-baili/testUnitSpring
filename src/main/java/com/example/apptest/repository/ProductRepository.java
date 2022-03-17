26b40374cea5a6fc56e17568f55c33141f1f29fd
hosni-baili <hosnibaili1987@gmail.com>
2022-03-17 15:14:06 +0100

    fix test

--- src/main/java/com/example/apptest/repository/ProductRepository.java
+++ src/main/java/com/example/apptest/repository/ProductRepository.java
@@ -1,7 +1,7 @@
-package com.example.apptest.repository;
-
-import com.example.apptest.entities.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product,Integer> {
-    public Product findByName(String name); }
+//package com.example.apptest.repository;
+//
+//import com.example.apptest.entities.Product;
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface ProductRepository extends CrudRepository<Product,Integer> {
+//    public Product findByName(String name); }
